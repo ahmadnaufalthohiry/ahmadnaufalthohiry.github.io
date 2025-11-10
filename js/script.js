573bf7561fac7fec950b764d27c2624bf2fa1e62
Ahmad Naufal Thohiry <ahmadnaufalthohiry@gmail.com>
2025-11-10 17:16:18 +0700

    Mengganti foto dan menambah sentuhan responsifitas pada aside

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const navToggler = document.querySelector(".nav-toggler");
+const aside = document.querySelector(".aside");
+
+navToggler.addEventListener("click", () => {
+  aside.classList.toggle("active");
+});
